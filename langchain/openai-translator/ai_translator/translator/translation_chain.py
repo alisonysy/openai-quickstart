40aa25ff478f2399ddbe94a6b4915719682e9739
alisonysy <qwen735@aucklanduni.ac.nz>
2024-12-30 21:31:13 +0800

    week8作业 chatglm2

--- langchain/openai-translator/ai_translator/translator/translation_chain.py
+++ langchain/openai-translator/ai_translator/translator/translation_chain.py
@@ -1,3 +1,4 @@
+from transformers import AutoTokenizer, AutoModel
 from langchain_openai import ChatOpenAI
 from langchain.chains import LLMChain
 
@@ -6,36 +7,49 @@ from langchain_core.prompts import ChatPromptTemplate, HumanMessagePromptTemplat
 
 class TranslationChain:
     def __init__(self, model_name: str = "gpt-3.5-turbo", verbose: bool = True):
-        
-        # 翻译任务指令始终由 System 角色承担
-        template = (
-            """You are a translation expert, proficient in various languages. \n
-            Translates {source_language} to {target_language}."""
-        )
-        system_message_prompt = SystemMessagePromptTemplate.from_template(template)
+        if model_name == '/kaggle/input/chatglm2/pytorch/6b/1':
+            # using chatglm2
+            # model deployed and running in kaggle
+            tokenizer = AutoTokenizer.from_pretrained(model_name, trust_remote_code=True)
+            model = AutoModel.from_pretrained(model_name, trust_remote_code=True).half().cuda()
+            self.model = model.eval()
+        else:
+            # 翻译任务指令始终由 System 角色承担
+            template = (
+                """You are a translation expert, proficient in various languages. \n
+                Translates {source_language} to {target_language}."""
+            )
+            system_message_prompt = SystemMessagePromptTemplate.from_template(template)
 
-        # 待翻译文本由 Human 角色输入
-        human_template = "{text}"
-        human_message_prompt = HumanMessagePromptTemplate.from_template(human_template)
+            # 待翻译文本由 Human 角色输入
+            human_template = "{text}"
+            human_message_prompt = HumanMessagePromptTemplate.from_template(human_template)
 
-        # 使用 System 和 Human 角色的提示模板构造 ChatPromptTemplate
-        chat_prompt_template = ChatPromptTemplate.from_messages(
-            [system_message_prompt, human_message_prompt]
-        )
+            # 使用 System 和 Human 角色的提示模板构造 ChatPromptTemplate
+            chat_prompt_template = ChatPromptTemplate.from_messages(
+                [system_message_prompt, human_message_prompt]
+            )
 
-        # 为了翻译结果的稳定性，将 temperature 设置为 0
-        chat = ChatOpenAI(model_name=model_name, temperature=0, verbose=verbose)
+            # 为了翻译结果的稳定性，将 temperature 设置为 0
+            chat = ChatOpenAI(model_name=model_name, temperature=0, verbose=verbose)
 
-        self.chain = LLMChain(llm=chat, prompt=chat_prompt_template, verbose=verbose)
+            self.chain = LLMChain(llm=chat, prompt=chat_prompt_template, verbose=verbose)
 
     def run(self, text: str, source_language: str, target_language: str) -> (str, bool):
         result = ""
         try:
-            result = self.chain.run({
-                "text": text,
-                "source_language": source_language,
-                "target_language": target_language,
-            })
+            if self.model:
+                prompt = (
+                    """You are a translation expert, proficient in various languages. \n
+                    Translates {source_language} to {target_language}."""
+                )
+                result, history = self.model.chat(tokenizer, prompt, max_length=2048, history=[])
+            elif self.chain:
+                result = self.chain.run({
+                    "text": text,
+                    "source_language": source_language,
+                    "target_language": target_language,
+                })
         except Exception as e:
             LOG.error(f"An error occurred during translation: {e}")
             return result, False
